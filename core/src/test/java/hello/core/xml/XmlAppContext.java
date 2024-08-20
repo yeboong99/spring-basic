bade8a8ec789abfbf90309053b67704f3b88413c
yeboong99 <yechani9905@gmail.com>
2024-08-21 04:53:13 +0900

    :wrench:[Feat] 추가 테스트 xml로 빈 설정해보기 xml을 이용하는 방식으로도 빈에 대해 설정이 가능하다. 설정파일의 모습은 resources/appConfig.xml파일에서 보면 된다. 테스트하기 위해 xmlAppContext파일 추가. 설정 대상 파일을 GenericXmlApplicationContext클래스를 이용해 (ApplicationContext의 자식임) 불러와 설정 파일로 이용한다. 이처럼 스프링은 다양한 형식의 설정 방법을 지원해준다.
    꼬리말 없음

---
+++ core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,17 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml"); // resources 디렉토리에서 appConfig.xml파일을 찾아 설정 파일로 사용
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
